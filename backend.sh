90570080d3c09205146406c622cc48c6bcfda136
sauja <pkpadwekar21@gmail.com>
2024-04-30 14:19:48 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -3,22 +3,20 @@ dnf module enable nodejs:18 -y
 
 dnf install nodejs -y
 
+cp backend.service /etc/systemd/system/backend.service
+
 useradd expense
 
 mkdir /app
 
-cp backend.service /etc/systemd/system/backend.service
-
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+
 cd /app
 unzip /tmp/backend.zip
 
-
 npm install
 
-
 systemctl daemon-reload
-
 systemctl enable backend
 systemctl start backend
 
--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,7 @@
 dnf install nginx -y
 
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl enable nginx
 systemctl start nginx
 
@@ -7,14 +9,9 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-cp expense.conf /etc/nginx/default.d/expense.conf
-
-
 
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-
-
 
 systemctl restart nginx
 
