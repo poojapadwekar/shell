6395b95a85e765c89abb55e1724d0c847563823e
sauja <pkpadwekar21@gmail.com>
2024-04-30 14:07:42 +0530

    expense shell

--- frontend.sh
+++ frontend.sh
@@ -7,7 +7,7 @@ rm -rf /usr/share/nginx/html/*
 
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
-cp /expense.conf /etc/nginx/default.d/expense.conf
+cp expense.conf /etc/nginx/default.d/expense.conf
 
 
 
