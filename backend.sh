91722cb0512ccd15e507f496dfbbfce47d8b9d67
sauja <pkpadwekar21@gmail.com>
2024-04-30 13:55:18 +0530

    expense shell

--- backend.sh
+++ backend.sh
@@ -24,6 +24,6 @@ systemctl start backend
 
 dnf install mysql -y
 
-mysql -h <MYSQL-SERVER-IPADDRESS> -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.21.24 -uroot -pExpenseApp@1 < /app/schema/backend.sql
 
 
