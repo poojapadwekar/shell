4f69e15544ba8d0664463aea7922223b340f0de5
sauja <pkpadwekar21@gmail.com>
2024-05-01 14:38:02 +0530

    Merge remote-tracking branch 'origin/main'

--- backend.sh
+++ backend.sh
@@ -1,28 +1,41 @@
+log_file=/tmp/expense.log
+
+echo Install NodeJS Repos
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
-dnf install nodejs -y
+dnf install nodejs -y >>$log_file
 
-cp backend.service /etc/systemd/system/backend.service
+echo Copy Backend Service
+cp backend.service /etc/systemd/system/backend.service >>$log_file
 
-useradd expense
-rm -rf /app
+echo Add ApplicatiOn User
+useradd expense >>$log_file
+
+echo Clear App Content
+rm -rf /app >>$log_file
 
 mkdir /app
+
+echo Download App Content
+curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip >>$log_file
+
 cd /app
+echo Extract App Content
+unzip /tmp/backend.zip >>$log_file
 
-curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+echo Download Dependencies
+npm install >>$log_file
 
-unzip /tmp/backend.zip
+echo Start Backend Service
+systemctl daemon-reload >>$log_file
+systemctl enable backend >>$log_file
+systemctl start backend >>$log_file
 
-npm install
+echo Install MySQL Client
+dnf install mysql -y >>$log_file
 
-systemctl daemon-reload
-systemctl enable backend
-systemctl start backend
-
-dnf install mysql -y
-
-mysql -h mysql.saujpoo.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo Load Schema
+mysql -h mysql.saujpoo.online -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
 
 
