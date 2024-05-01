47c47e27f0335f5fe068ae48a070986595356774
sauja <pkpadwekar21@gmail.com>
2024-05-01 14:12:00 +0530

    Merge remote-tracking branch 'origin/main'

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,22 @@
-dnf install nginx -y
+log_file=/tmp/expense.log
+echo Installing nginx
+dnf install nginx -y >>log_file
 
-cp expense.conf /etc/nginx/default.d/expense.conf
+echo Placing Expense Config file in Nginx
+cp expense.conf /etc/nginx/default.d/expense.conf >>log_file
 
-systemctl enable nginx
-systemctl start nginx
+echo Removing old Nginx content
+rm -rf /usr/share/nginx/html/* >>log_file
 
-rm -rf /usr/share/nginx/html/*
-
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
+echo Download Frontend Code
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip >>log_file
 
 
 cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
 
-systemctl restart nginx
+echo Extracting frontend code
+unzip /tmp/frontend.zip >>log_file
 
+echo Starting Nginx Service
+systemctl enable nginx >>log_file
+systemctl restart nginx >>log_file
