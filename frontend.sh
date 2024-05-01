a41dcdc20882c93e87b83f57e5d08d2bb1d4cdbc
sauja <pkpadwekar21@gmail.com>
2024-05-01 19:41:47 +0530

    Merge remote-tracking branch 'origin/main'

--- frontend.sh
+++ frontend.sh
@@ -3,26 +3,26 @@ component=frontend
 
 echo Installing nginx
 dnf install nginx -y &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Placing Expense Config file in Nginx
 cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Removing old Nginx content
 rm -rf /usr/share/nginx/html/* &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 cd /usr/share/nginx/html
-stat_check &>>$log_file
+stat_check
 
 download_and_extract &>>$log_file#function is called
-stat_check &>>$log_file
+stat_check
 
 
 echo Starting Nginx Service
 systemctl enable nginx &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 systemctl restart nginx &>>$log_file
-stat_check &>>$log_file
+stat_check
