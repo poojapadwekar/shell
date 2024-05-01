a8246062894f0aaed171286ef3968b0b1105cc56
sauja <pkpadwekar21@gmail.com>
2024-05-01 15:46:18 +0530

    Merge remote-tracking branch 'origin/main'

--- backend.sh
+++ backend.sh
@@ -5,16 +5,16 @@ echo Install NodeJS Repos
 dnf module disable nodejs -y
 dnf module enable nodejs:18 -y
 
-dnf install nodejs -y >>$log_file
+dnf install nodejs -y &>>$log_file
 
 echo Copy Backend Service
-cp backend.service /etc/systemd/system/backend.service >>$log_file
+cp backend.service /etc/systemd/system/backend.service &>>$log_file
 
 echo Add ApplicatiOn User
-useradd expense >>$log_file
+useradd expense &>>$log_file
 
 echo Clear App Content
-rm -rf /app >>$log_file
+rm -rf /app &>>$log_file
 
 mkdir /app
 
@@ -23,17 +23,17 @@ cd /app
 download_and_extract #function is called
 
 echo Download Dependencies
-npm install >>$log_file
+npm install &>>$log_file
 
 echo Start Backend Service
-systemctl daemon-reload >>$log_file
-systemctl enable backend >>$log_file
-systemctl start backend >>$log_file
+systemctl daemon-reload &>>$log_file
+systemctl enable backend &>>$log_file
+systemctl start backend &>>$log_file
 
 echo Install MySQL Client
-dnf install mysql -y >>$log_file
+dnf install mysql -y &>>$log_file
 
 echo Load Schema
-mysql -h 172.31.21.0 -uroot -pExpenseApp@1 < /app/schema/backend.sql >>$log_file
+mysql -h 172.31.21.0 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
 
 
--- frontend.sh
+++ frontend.sh
@@ -2,18 +2,18 @@ source common.sh
 component=frontend
 
 echo Installing nginx
-dnf install nginx -y >>$log_file
+dnf install nginx -y &>>$log_file
 
 echo Placing Expense Config file in Nginx
-cp expense.conf /etc/nginx/default.d/expense.conf >>$log_file
+cp expense.conf /etc/nginx/default.d/expense.conf &>>$log_file
 
 echo Removing old Nginx content
-rm -rf /usr/share/nginx/html/* >>$log_file
+rm -rf /usr/share/nginx/html/* &>>$log_file
 
 cd /usr/share/nginx/html
 
 download_and_extract #function is called
 
 echo Starting Nginx Service
-systemctl enable nginx >>$log_file
-systemctl restart nginx >>$log_file
+systemctl enable nginx &>>$log_file
+systemctl restart nginx &>>$log_file
