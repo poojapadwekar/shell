e6b806b039f0bfa02745100fa2544e64938c06b0
sauja <pkpadwekar21@gmail.com>
2024-05-01 19:41:47 +0530

    Merge remote-tracking branch 'origin/main'

--- common.sh
+++ common.sh
@@ -10,11 +10,10 @@ download_and_extract() {
 
 
 stat_check() {
-
- if [ $? -eq 0 ]; then
-  echo -e "\e[32mSUCCESS\e[0m"
- else
-  echo -e "\e[32mSUCCESS\e[0m"
-  exit 1
- fi
+  if [ $? -eq 0 ]; then
+   echo -e "\e[32mSUCCESS\e[0m"
+  else
+   echo -e "\e[32mSUCCESS\e[0m"
+   exit 1
+  fi
 }
