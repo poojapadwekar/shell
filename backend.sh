82e8cf905be1d3290a6dae479b86643050627471
sauja <pkpadwekar21@gmail.com>
2024-05-01 19:58:04 +0530

    Merge remote-tracking branch 'origin/main'

--- backend.sh
+++ backend.sh
@@ -3,57 +3,57 @@ component=backend
 
 echo Install NodeJS Repos
 dnf module disable nodejs -y &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 dnf module enable nodejs:18 -y &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 dnf install nodejs -y &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Copy Backend Service
 cp backend.service /etc/systemd/system/backend.service &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Add ApplicatiOn User
 id expense &>>$log_file
 if [ $? -ne 0 ]; then
   useradd expense &>>$log_file
 fi
-stat_check &>>$log_file
+stat_check
 
 echo Clear App Content
 rm -rf /app &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 mkdir /app
-stat_check &>>$log_file
+stat_check
 
 cd /app
-stat_check &>>$log_file
+stat_check
 
 download_and_extract &>>$log_file#function is called
-stat_check &>>$log_file
+stat_check
 
 echo Download Dependencies
 npm install &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Start Backend Service
 systemctl daemon-reload &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 systemctl enable backend &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 systemctl start backend &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Install MySQL Client
 dnf install mysql -y &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Load Schema
 mysql -h 172.31.21.0 -uroot -pExpenseApp@1 < /app/schema/backend.sql &>>$log_file
-stat_check &>>$log_file
+stat_check
 
--- mysql.sh
+++ mysql.sh
@@ -2,27 +2,27 @@ source common.sh
 
 echo Disable MySQL 8 Version
 dnf module disable mysql -y &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Copy MySQL Repo File
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Install MySQL Server
 dnf install mysql-community-server -y &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 echo Start MySQL Service
 systemctl enable mysqld &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 systemctl start mysqld &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 
 echo Setup root Password
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-stat_check &>>$log_file
+stat_check
 
 
 
